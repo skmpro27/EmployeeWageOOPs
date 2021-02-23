aa9393173f70fc7a029b6c62c335b6c7f57e4d93
unknown <kumar.skm03@gmail.com>
2021-02-23 13:11:00 +0530

    added function is employee peresent in EmpWageOOPs.java

--- EmpWageOOPs.java
+++ EmpWageOOPs.java
@@ -1,7 +1,26 @@
 
 public class EmpWageOOPs {
-	public static void main(String args[]) {
+
+	public static final int IS_EMPLOYEE_PRESENT = 1;
+
+	public EmpWageOOPs() {
 
 		System.out.println("Welcome to Employee Wages Computation Program");
+	}
+
+	public void IsEmpPresent() {
+
+		int empCheck = (int) (Math.random() * 2);
+		if (empCheck == IS_EMPLOYEE_PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+
+	public static void main(String args[]) {
+
+		EmpWageOOPs obj = new EmpWageOOPs();
+		obj.IsEmpPresent();
+
 	}
 }
