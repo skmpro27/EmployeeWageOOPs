0d17909a5ebde77b04f263b9c184ac5bbb4d2d6f
unknown <kumar.skm03@gmail.com>
2021-02-23 13:52:29 +0530

    added part time employee in EmpWageOOPs.java

--- EmpWageOOPs.java
+++ EmpWageOOPs.java
@@ -1,7 +1,8 @@
 
 public class EmpWageOOPs {
 
-	public static final int IS_EMPLOYEE_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public final int WAGE_PER_HOUR;
 
 	public EmpWageOOPs(int perHr) {
@@ -13,11 +14,14 @@ public class EmpWageOOPs {
 	public int EmpWage() {
 
 		int empHrs = 0;
-		int empCheck = (int) (Math.random() * 2);
-		if (empCheck == IS_EMPLOYEE_PRESENT)
+		int empCheck = (int) (Math.random() * 3);
+		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
+
 		return empHrs * WAGE_PER_HOUR;
 	}
 
