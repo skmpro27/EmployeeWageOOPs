776daab374f3a0948205a54b43402e03a7192bd9
unknown <kumar.skm03@gmail.com>
2021-02-23 13:40:52 +0530

    added function EmpWage for daily wage in EmpWageOOPs.java

--- EmpWageOOPs.java
+++ EmpWageOOPs.java
@@ -2,25 +2,28 @@
 public class EmpWageOOPs {
 
 	public static final int IS_EMPLOYEE_PRESENT = 1;
+	public final int WAGE_PER_HOUR;
 
-	public EmpWageOOPs() {
+	public EmpWageOOPs(int perHr) {
 
 		System.out.println("Welcome to Employee Wages Computation Program");
+		WAGE_PER_HOUR = perHr;
 	}
 
-	public void IsEmpPresent() {
+	public int EmpWage() {
 
+		int empHrs = 0;
 		int empCheck = (int) (Math.random() * 2);
 		if (empCheck == IS_EMPLOYEE_PRESENT)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		return empHrs * WAGE_PER_HOUR;
 	}
 
 	public static void main(String args[]) {
 
-		EmpWageOOPs obj = new EmpWageOOPs();
-		obj.IsEmpPresent();
-
+		EmpWageOOPs obj = new EmpWageOOPs(20);
+		System.out.println("Employee daily wage: " + obj.EmpWage());
 	}
 }
